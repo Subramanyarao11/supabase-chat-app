fcb2c82918a752435eaee5c40c39ee59202848e7
Subramanyarao11 <subramanya11rao@gmail.com>
2023-03-26 18:28:56 +0530

    supabase working

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+import { supabase } from './supabaseClient'
 function App() {
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        getMessages();
+      }, []);
+
+      async function getMessages() {
+        const { data } = await supabase.from("messages").select();
+        setMessages(data);
+      }
 
   return (
-    <div className="text-center">Hello world</div>
+    <>
+    <div className="text-center">Hello from Supabase!</div>
+    <ul>
+        {messages && messages.map((message) => (
+          <li key={message.id}>{message.message}</li>
+        ))}
+      </ul>
+      </>
   )
 }
 
